cfca9ded19b652dd9e1c2e8c3947721f619ce039
Abhinav Tiwari <abhinavt82@gmail.com>
2013-10-20 11:44:30 -0500

    Introduced login locale under routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 KelloggCafe::Application.routes.draw do
+  get("/login", { :controller => "login", :action => "authenticate"})
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
